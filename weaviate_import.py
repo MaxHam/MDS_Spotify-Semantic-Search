fc6dfbbc40255346ff00254e5ccc2f31ae718edb
Max <max.hammer1998@googlemail.com>
2023-05-17 12:45:38 +0200

    add more schema classes(do not use them

--- weaviate_import.py
+++ weaviate_import.py
@@ -1,28 +1,32 @@
 import weaviate
 import json
 
+classes= ['Track']#, 'Artist', 'Album']
+
 def init_client():
     client = weaviate.Client(
         url="http://localhost:8080",  # Replace with your endpoint
     )
 
     # Import class schemas
-    with open("schema/track_class.json", "r") as f:
-        track_class_obj = json.load(f)
-    # Create classes
-    classes = [track_class_obj]
+    class_paths = ["schema/track_class.json"] #, "schema/artist_class.json", "schema/album_class.json"]
 
-    # Create classes
-    try:
-        for c in classes:
-            client.schema.create_class(c)
-    except:
-        print("class already exists")
+    for path in class_paths:
+        with open(path, "r") as f:
+            class_obj = json.load(f)
+        # Create classes
+        try:
+            client.schema.create_class(class_obj)
+        except:
+            print(f"class {path} already exists")
     
     return client
 
 def delete_class(client, class_name="Songs"):
-    client.schema.delete_class(class_name)
+    try:
+        client.schema.delete_class(class_name)
+    except:
+        print(f"class {class_name} does not exist")
 
 def read_data():
     # Read the data
@@ -31,22 +35,21 @@ def read_data():
     return data
 
 def import_data(client, data):
-    # Configure a batch process
+    # cut data
+    # data = data[:10]
+
+    # Configure a batch 
     with client.batch as batch:
         batch.batch_size=100
         # Batch import all tracks
         for i, d in enumerate(data):
             print(f"importing track: {i+1}")
 
-            properties = {
-                "track_name": d["track_name"],
-                "track_artist": d["track_artist"],
+            track_properties = {
+                "name": d["track_name"],
                 "track_id": d["track_id"],
                 "lyrics": d["lyrics"],
-                "track_popularity": d["track_popularity"],
-                "album_id": d["track_album_id"],
-                "album_name": d["track_album_name"],
-                "album_release_date": d["track_album_release_date"],
+                "popularity": d["track_popularity"],
                 "genre": d["playlist_genre"],
                 "subgenre": d["playlist_subgenre"],
                 "danceability": d["danceability"],
@@ -61,12 +64,29 @@ def import_data(client, data):
                 "valence": d["valence"],
                 "tempo": d["tempo"],
                 "duration_ms": d["duration_ms"],
-            }
-            client.batch.add_data_object(properties, "Track")
+                "album": d["track_album_name"],
+                "album_id": d["track_album_id"],
+                "releas_date": d["track_album_release_date"],
+                "artist": d["track_artist"]
+                }
+
+            # artist_properties = {
+            #     "name": d["track_artist"]
+            # }
+
+            # album_properties = {
+            #     "name": d["track_album_name"],
+            #     "album_id": d["track_album_id"],
+            #     "releas_date": d["track_album_release_date"],
+            # }
+
+            client.batch.add_data_object(track_properties, "Track")
+            # client.batch.add_data_object(artist_properties, "Artist")
+            # client.batch.add_data_object(album_properties, "Album")
 
 def query(client):
     where_filter = {
-        "path": ["track_popularity"],
+        "path": ["popularity"],
         "operator": "GreaterThan",
         "valueNumber": "80"
     }
@@ -76,7 +96,7 @@ def query(client):
 
     result = ( 
         client.query
-        .get("Track", ["track_name", "track_artist", "track_popularity"])
+        .get("Track", ["name", "lyrics", "popularity"])
         .with_where(where_filter)
         .with_near_text(nearText)
         .with_limit(2)
@@ -85,12 +105,30 @@ def query(client):
 
     print(json.dumps(result, indent=4))
 
+def ask(client):
+    ask = {
+        "question": "Ariana Grande?",
+        "properties": ["lyrics", "name"],
+    }
+
+    result = (
+        client.query
+        .get("Track", ["name", "_additional {answer {hasAnswer certainty property result startPosition endPosition} }"])
+        .with_ask(ask)
+        .with_limit(1)
+        .do()
+        )
+    print(json.dumps(result, indent=4))
+
 def main(args):
     client = init_client()
-    # delete_class(client, 'Track')
-    # data = read_data()  
-    # import_data(client, data)
-    # query(client)
+    for c in classes:
+        delete_class(client, c)
+
+    data = read_data()  
+    import_data(client, data)
+    query(client)
+    ask(client)
 
 
 
