bb0cc0f57a5ad18a89c5a9ddb06143c1849906f7
Max <max.hammer1998@googlemail.com>
2023-05-25 15:36:55 +0200

    add webapp to docker-compose

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -6,12 +6,18 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    host: true,
+    strictPort: true,
     proxy: {
       '/auth': {
           target: 'http://localhost:5001',
           changeOrigin: true,
           secure: false
       }
+    },
+    // add the next lines if you're using windows and hot reload doesn't work
+    watch: {
+      usePolling: true
     }
   },
   
